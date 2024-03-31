6a505bf98578e27d70025eeff5265c3ccab4157f
Uday Singh <uday144@gmail.com>
2024-03-31 14:37:32 +1100

    updated UTs

--- app/src/androidTest/java/com/store/mvvmtest/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/store/mvvmtest/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.cheezycode.mvvmtest", appContext.packageName)
+        assertEquals("com.store.mvvmtest", appContext.packageName)
     }
 }
